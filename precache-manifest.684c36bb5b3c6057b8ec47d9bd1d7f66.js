9aaab4c20cbec784182f5f296b2a6c2459da8e2a
abdellani <abdellani@gmail.com>
2020-03-03 14:18:33 +0100

    Updates

---
+++ precache-manifest.684c36bb5b3c6057b8ec47d9bd1d7f66.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b06071d9f96a06ae3306d8df0ad7d8fa",
+    "url": "/interview-frontend-task/index.html"
+  },
+  {
+    "revision": "22ba5f1d2efb38c50142",
+    "url": "/interview-frontend-task/static/css/main.f6bb8430.chunk.css"
+  },
+  {
+    "revision": "c066f7d1b4d09d56c8dd",
+    "url": "/interview-frontend-task/static/js/2.21de55a5.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/interview-frontend-task/static/js/2.21de55a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22ba5f1d2efb38c50142",
+    "url": "/interview-frontend-task/static/js/main.9a7b165b.chunk.js"
+  },
+  {
+    "revision": "cce23d42553569283e89",
+    "url": "/interview-frontend-task/static/js/runtime-main.616d8c15.js"
+  },
+  {
+    "revision": "4f4f912d1cd59d0a2d97d4620bf67e31",
+    "url": "/interview-frontend-task/static/media/green.4f4f912d.png"
+  },
+  {
+    "revision": "4ddb8934062953d190f7753e3a1eec07",
+    "url": "/interview-frontend-task/static/media/red.4ddb8934.png"
+  }
+]);
